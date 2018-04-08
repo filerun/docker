9e7ebfdf8ff5686be321eb396e7a602c7d3d3fae
Roelof Jansen <roelof@rena-it.nl>
2018-04-08 17:11:06 +0200

    Fix for DB port

--- autoconfig.php
+++ autoconfig.php
@@ -1,7 +1,7 @@
 <?php 
 $config['db'] = [
   'type' => getenv('DB_TYPE') ?: 'mysql',
-  'server' => getenv('DB_HOST') ?: 'db',
+  'server' => sprintf('%s:%s', getenv('DB_HOST') ?: 'db', getenv('DB_PORT') ?: '3306'),
   'database' => getenv('DB_NAME') ?: 'filerun',
   'username' => getenv('DB_USER') ?: 'filerun',
   'password' => getenv('DB_PASS') ?: 'filerun'
