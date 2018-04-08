4c1758aa31a2ef9186e81467a637ab507a50f175
Roelof Jansen <roelof@rena-it.nl>
2018-04-08 16:58:33 +0200

    Fix for DB name

--- import-db.sh
+++ import-db.sh
@@ -1,1 +1,1 @@
-mysql --user=$DB_USER --password=$DB_PASS --host=$DB_HOST --port=$DB_PORT filerun < /filerun.setup.sql
+mysql --user=$DB_USER --password=$DB_PASS --host=$DB_HOST --port=$DB_PORT $DB_NAME < /filerun.setup.sql
