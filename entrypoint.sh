e90a481994742b567850609c7468bb8b75be0d25
Daniel Baum <dbaum@debian.baum.lan>
2018-07-14 19:33:53 -0400

    Checking for user exist and creating if needed

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 set -eux
 
-groupadd --gid ${APACHE_RUN_GROUP_ID} ${APACHE_RUN_GROUP}
-useradd --uid ${APACHE_RUN_USER_ID} --gid ${APACHE_RUN_GROUP_ID} ${APACHE_RUN_USER}
+# Check if user exists
+if ! id -u ${APACHE_RUN_USER} > /dev/null 2>&1; then
+	echo "The user ${APACHE_RUN_USER} does not exist, creating..."
+	groupadd --gid ${APACHE_RUN_GROUP_ID} ${APACHE_RUN_GROUP}
+	useradd --uid ${APACHE_RUN_USER_ID} --gid ${APACHE_RUN_GROUP_ID} ${APACHE_RUN_USER}
+fi
 
 if [ ! -e /var/www/html/index.php ];  then
 	echo "[FileRun fresh install]"
@@ -13,7 +17,6 @@ if [ ! -e /var/www/html/index.php ];  then
 	mysql_host="${FR_DB_HOST:-mysql}"
 	mysql_port="${FR_DB_PORT:-3306}"
 	/wait-for-it.sh $mysql_host:$mysql_port -t 120 -- /import-db.sh
-
 fi
 
 exec "$@"
