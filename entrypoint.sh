9c97a60eae81d1212d92b19cd5dd377f53df8399
Makeshift <Me@Connor-Bell.com>
2020-03-26 01:35:08 +0000

    Moved import-db step to the top of the first-run installation block so a failed DB connection on first run doesn't cause the setup to fail to continue next time

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,13 +11,13 @@ fi
 # Install FileRun on first run
 if [ ! -e /var/www/html/index.php ];  then
 	echo "[FileRun fresh install]"
+	mysql_host="${FR_DB_HOST:-mysql}"
+	mysql_port="${FR_DB_PORT:-3306}"
+	/wait-for-it.sh $mysql_host:$mysql_port -t 120 -- /import-db.sh
 	unzip /filerun.zip -d /var/www/html/
 	cp /autoconfig.php /var/www/html/system/data/
 	chown -R ${APACHE_RUN_USER}:${APACHE_RUN_GROUP} /var/www/html
 	chown ${APACHE_RUN_USER}:${APACHE_RUN_GROUP} /user-files
-	mysql_host="${FR_DB_HOST:-mysql}"
-	mysql_port="${FR_DB_PORT:-3306}"
-	/wait-for-it.sh $mysql_host:$mysql_port -t 120 -- /import-db.sh
 fi
 
 exec "$@"
