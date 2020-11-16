20541b518d53ae3aeccaf930bc4a13aa538caa43
Sean Lee <leejoneshane@gmail.com>
2020-11-16 12:28:56 +0800

    Update import-db.sh

--- import-db.sh
+++ import-db.sh
@@ -1,1 +1,7 @@
+if mysqlshow --host=$FR_DB_HOST --port=$FR_DB_PORT --user=$FR_DB_USER --password=$FR_DB_PASS $FR_DB_NAME; then
+    echo "database exist!"
+else
+    echo "CREATE DATABASE $FR_DB_NAME CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;" | mysql --host=$FR_DB_HOST --port=$FR_DB_PORT --user=$FR_DB_USER --password=$FR_DB_PASS
+fi
+
 mysql --user=$FR_DB_USER --password=$FR_DB_PASS --host=$FR_DB_HOST --port=$FR_DB_PORT $FR_DB_NAME < /filerun.setup.sql
