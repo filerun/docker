cecf239ffc020eec001c424a13a5ac98405ebc21
FileRun <filerun@users.noreply.github.com>
2022-01-08 18:22:26 +0000

    Delete autoconfig.php

--- autoconfig.php
+++
@@ -1,8 +0,0 @@
-<?php
-$config['db'] = [
-	'server' => getenv('FR_DB_HOST') .';port='. getenv('FR_DB_PORT'),
-	'database' => getenv('FR_DB_NAME'),
-	'username' => getenv('FR_DB_USER'),
-	'password' => getenv('FR_DB_PASS'),
-	'sync_timezone' => false
-];
