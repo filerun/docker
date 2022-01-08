3fd047ab26c36b4b8c54cffd4945d82ae8205469
FileRun <filerun@users.noreply.github.com>
2022-01-08 18:23:31 +0000

    Delete import-db.sh

--- import-db.sh
+++
@@ -1,6 +0,0 @@
-if mysql --user=$FR_DB_USER --password=$FR_DB_PASS --host=$FR_DB_HOST --port=$FR_DB_PORT $FR_DB_NAME -e "select \`val\` from df_settings where \`var\`='currentVersion'"; then
-  echo "Using existing database!"
-else
-  echo "Creating fresh FileRun database!"
-  mysql --user=$FR_DB_USER --password=$FR_DB_PASS --host=$FR_DB_HOST --port=$FR_DB_PORT $FR_DB_NAME < /filerun.setup.sql
-fi
